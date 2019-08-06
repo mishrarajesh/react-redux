1a2019c2ad0578b093b3fb57a3e0db975af08dda
Rajesh Kumar Mishra <rmishra@rsamindia.local>
2019-08-07 00:29:00 +0530

    first local to repository

---
+++ src/components/CoursesPage.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import {connect} from 'react-redux';
+import * as courseActions  from '../redux/actions/courseActions';
+
+class CoursesPage extends React.Component{
+
+    state={
+        course:{
+            title:""
+        }
+    };
+
+
+    handleChange = (event) => {
+        const course = {...this.state.course, title:event.target.value};
+        this.setState({course});
+    }
+
+    handleSubmit = (event) => {
+        event.preventDefault();
+        this.props.dispatch(courseActions.createCourse(this.state.course));
+    }
+
+    render(){
+        return(
+            <form onSubmit={this.handleSubmit}>
+                <h2>Courses</h2>
+                <h3>Add Course</h3>
+                <input type="text" onChange={this.handleChange} 
+                value={this.state.course.title} />
+
+                <input type="submit" value="Save" />
+                {this.props.courses.map(course => (
+                    <div key={course.title}>{course.title}</div>
+                ))}
+            </form>
+        );
+    }
+
+
+}
+
+//what state we want to expose to our component
+function mapStateToProps(state, ownProps){
+    return{
+        courses  :state.courses
+    }
+}
+
+//what aciton we want to expose to our component
+function mapDispatchToProps(){
+
+}
+export default connect(mapStateToProps) (CoursesPage);
